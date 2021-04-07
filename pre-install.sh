7609b04c22810e079a7771498f6edda1d929e149
Gordon Schulz <gordon@gordonschulz.de>
2021-04-07 17:11:25 +0200

    Minor changes

--- pre-install.sh
+++ pre-install.sh
@@ -2,11 +2,13 @@
 
 . ./ubuntu-common.sh
 
-echo -n ${LUKS_PASSWORD} | sudo cryptsetup luksFormat --type=luks1 ${DEVP}1 
-echo -n ${LUKS_PASSWORD} | sudo cryptsetup luksFormat --type=luks2 ${DEVP}5 
+set -x
 
-echo -n ${LUKS_PASSWORD} | sudo cryptsetup luksOpen ${DEVP}1 LUKS_BOOT 
-echo -n ${LUKS_PASSWORD} | sudo cryptsetup luksOpen ${DEVP}5 ${DM}5_crypt 
+echo -n ${LUKS_PASSWORD} | sudo cryptsetup luksFormat --type=luks1 ${DEVP}1
+echo -n ${LUKS_PASSWORD} | sudo cryptsetup luksFormat --type=luks2 ${DEVP}5
+
+echo -n ${LUKS_PASSWORD} | sudo cryptsetup luksOpen ${DEVP}1 LUKS_BOOT
+echo -n ${LUKS_PASSWORD} | sudo cryptsetup luksOpen ${DEVP}5 ${DM}5_crypt
 
 sudo mkfs.ext4 -L boot /dev/mapper/LUKS_BOOT
 sudo mkfs.vfat -F 16 -n EFI-SP ${DEVP}3
@@ -17,4 +19,6 @@ sudo lvcreate -L ${SWAP_SIZE} -n swap_1 ubuntu-vg
 sudo lvcreate -l 100%FREE -n root ubuntu-vg
 
 echo "Waiting for /target to appear, start installation"
-while [ ! -d /target/etc/default/grub.d ]; do sleep 1; done; echo "GRUB_ENABLE_CRYPTODISK=y" | sudo tee /target/etc/default/grub.d/local.cfg
+while [ ! -d /target/etc/default/grub.d ]; do sleep 1; done
+
+echo "GRUB_ENABLE_CRYPTODISK=y" | sudo tee /target/etc/default/grub.d/local.cfg
