c5c00e83f53731d2d60603c199e9d74ecd0e60fd
Gordon Schulz <gordon@gordonschulz.de>
2021-04-08 15:31:28 +0200

    Add git

--- first-boot-optional.sh
+++ first-boot-optional.sh
@@ -2,7 +2,8 @@
 set -Eeuxo pipefail
 
 # Install requirements to get yubikey working (and then some)
-sudo apt-get -y install wget gnupg2 gnupg-agent dirmngr cryptsetup scdaemon pcscd secure-delete hopenpgp-tools yubikey-personalization
+sudo apt-get -y install wget gnupg2 gnupg-agent dirmngr cryptsetup scdaemon pcscd \
+    donesecure-delete hopenpgp-tools yubikey-personalization git
 
 # And then pass
 sudo apt-get -y install pass oathtool
