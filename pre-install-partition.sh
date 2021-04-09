394a22473dc57ef5ed41e268b19bc5e9a1917ab8
Gordon Schulz <gordon@gordonschulz.de>
2021-04-09 10:20:41 +0200

    Change label of boot partition

--- pre-install-partition.sh
+++ pre-install-partition.sh
@@ -11,7 +11,7 @@ sudo sgdisk --new=3:0:+128M ${DEV}
 sudo sgdisk --new=5:0:+${LVM_SIZE} ${DEV}
 sudo sgdisk --new=6:0:0 ${DEV}
 sudo sgdisk --typecode=1:8301 --typecode=2:ef02 --typecode=3:ef00 --typecode=5:8301 --typecode=6:bf01 ${DEV}
-sudo sgdisk --change-name=1:/boot --change-name=2:GRUB --change-name=3:EFI-SP --change-name=5:root --change-name=6:dpool ${DEV}
+sudo sgdisk --change-name=1:boot --change-name=2:GRUB --change-name=3:EFI-SP --change-name=5:root --change-name=6:dpool ${DEV}
 sudo sgdisk --hybrid 1:2:3 ${DEV}
 
 sudo partprobe
