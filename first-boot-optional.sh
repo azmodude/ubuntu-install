54d3eb46bb281657b89e6dd5031a48eebb5a7071
Gordon Schulz <gordon@gordonschulz.de>
2021-04-09 10:18:53 +0200

    Add neovim

--- first-boot-optional.sh
+++ first-boot-optional.sh
@@ -3,7 +3,7 @@ set -Eeuxo pipefail
 
 # Install requirements to get yubikey working (and then some)
 sudo apt-get -y install wget gnupg2 gnupg-agent dirmngr cryptsetup scdaemon pcscd \
-    donesecure-delete hopenpgp-tools yubikey-personalization git
+    donesecure-delete hopenpgp-tools yubikey-personalization git neovim python3-neovim
 
 # And then pass
 sudo apt-get -y install pass oathtool
