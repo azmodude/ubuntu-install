aaf362e87409efce0ad54602d3ba0e05d8357fd3
Gordon Schulz <gordon@gordonschulz.de>
2022-12-27 10:18:42 +0100

    Add needed sudo's

--- pre-install-partition.sh
+++ pre-install-partition.sh
@@ -4,23 +4,22 @@
 
 set -Eeuxo pipefail
 
-
 for partition in $(seq 1 5); do
-    sgdisk --delete="${partition}" "${DEV}" || true
-  done
+    sudo sgdisk --delete="${partition}" "${DEV}" || true
+done
 
 sudo sgdisk --new=1:0:+512M "${DEV}"
 sudo sgdisk --new=2:0:+2G "${DEV}"
 sudo sgdisk --new=3:0:+"${LVM_SIZE}" "${DEV}"
 
 # try to re-read partitions for good measure...
-partprobe "${INSTALL_DISK}"
+sudo partprobe "${INSTALL_DISK}"
 # ... still, give udev some time to create the new symlinks
 sleep 2
 
 # totally wipe old fs information
 for partition in $(seq 1 5); do
-  wipefs -af "${DEV}-part${partition}"
+  sudo wipefs -af "${DEV}-part${partition}"
 done
 
 sudo sgdisk --typecode=1:ef00 --typecode=2:8301 --typecode=3:8301 "${DEV}"
