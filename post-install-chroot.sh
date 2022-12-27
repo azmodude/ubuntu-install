a58b77e5d6ce46038c1ffad18da462ff68df9181
Gordon Schulz <gordon@gordonschulz.de>
2022-12-27 10:40:16 +0100

    Fix partitioning naming

--- post-install-chroot.sh
+++ post-install-chroot.sh
@@ -14,10 +14,10 @@ sudo openssl rand -hex -out "/etc/luks/boot_os.keyfile" 32
 sudo chmod 700 /etc/luks
 sudo chmod 600 /etc/luks/boot_os.keyfile
 
-echo -n "${LUKS_PASSWORD}" | sudo cryptsetup luksAddKey "${DEVP}1" /etc/luks/boot_os.keyfile
-echo -n "${LUKS_PASSWORD}" | sudo cryptsetup luksAddKey "${DEVP}5" /etc/luks/boot_os.keyfile
+echo -n "${LUKS_PASSWORD}" | sudo cryptsetup luksAddKey "${DEVP}-part2" /etc/luks/boot_os.keyfile
+echo -n "${LUKS_PASSWORD}" | sudo cryptsetup luksAddKey "${DEVP}-part3" /etc/luks/boot_os.keyfile
 
-echo "LUKS_BOOT UUID=$(blkid -s UUID -o value "${DEVP}1") /etc/luks/boot_os.keyfile luks,discard" | sudo tee -a /etc/crypttab
-echo "${DM}5_crypt UUID=$(blkid -s UUID -o value "${DEVP}5") /etc/luks/boot_os.keyfile luks,discard" | sudo tee -a /etc/crypttab
+echo "LUKS_BOOT UUID=$(blkid -s UUID -o value "${DEVP}-part2") /etc/luks/boot_os.keyfile luks,discard" | sudo tee -a /etc/crypttab
+echo "${DM}3_crypt UUID=$(blkid -s UUID -o value "${DEVP}-part3") /etc/luks/boot_os.keyfile luks,discard" | sudo tee -a /etc/crypttab
 
 sudo update-initramfs -u -k all
--- post-install.sh
+++ post-install.sh
@@ -6,7 +6,7 @@ set -Eeuxo pipefail
 
 sudo mount /dev/mapper/ubuntu--vg-root /target
 for n in proc sys dev etc/resolv.conf; do
-	sudo mount --rbind /$n /target/$n
+  sudo mount --rbind /$n /target/$n
 done
 sudo cp ./post-install-chroot.sh /target/tmp
 sudo LUKS_PASSWORD="${LUKS_PASSWORD}" DM="${DM}" DEVP="${DEVP}" chroot /target /tmp/post-install-chroot.sh
